e9541964ab36c7d18b4d864b7ee83a15bda49255
Sako Afonso Miezi Vuna <61125457+Afonso-sk@users.noreply.github.com>
2022-04-14 12:07:53 +0100

    Primeiro  commit

---
+++ sintaxe/sintaxe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Sintaxe básica do C-SHARP.
+
+namespace sintaxe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.writeLne(), é o método que serve para imprimir o texto na tela,mas se não colocarmos 
+             Console.ReadLine o texto que imprime na tela some muito Rápido.*/
+            Console.WriteLine("Olá, chamo-me Sako Afonso Miezi Vuna ! ");
+            Console.WriteLine("3 + 3");
+            Console.ReadLine();
+            /*1- Using system: significa que podemos usar as classes do system namespace
+              2- namespace: é usada para organoizar o nosso código
+              3- {} : Marcam o inicio e o fim do codigo.
+              4- Class : é um container para todos os métodos, que traz funcionalidades 
+            ao seu programa.
+            */
+        }
+    }
+}
