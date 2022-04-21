60ce9128ae765ac21154a4426240f8879b235042
Sako Afonso Miezi Vuna <61125457+Afonso-sk@users.noreply.github.com>
2022-04-21 16:14:30 +0100

    Quintafeira2104202216hs

---
+++ sintaxe/chaveeuromilgoes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//gerar chaves aleatorios de euromilhoes
+
+namespace chaveeuromilgoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+
+            Console.WriteLine("Quantas chaves pretendes gerar ? ");
+            int num_chaves = Convert.ToInt32(Console.ReadLine());
+
+            while (num_chaves > 0)
+            {
+
+                int numeros = 5; 
+
+                while (numeros > 0)
+                {
+
+                    Console.Write($"{aleatorio.Next(1, 50)} ");
+                    numeros--;
+                }
+                Console.Write(" - ");
+
+                //numeros de estrelas
+                int estrelas = 2;
+
+                while (estrelas > 0)
+                {
+                    Console.Write($"{aleatorio.Next(1, 12)} ");
+                    estrelas--;
+                }
+
+                num_chaves--;
+                Console.WriteLine("\n"); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sintaxe/notaformando/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Calcular a nota dos formandos
+
+namespace notaformando
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num_formando, num_formando_exato;
+            double nota_formando;
+            double media;
+            double total = 0;
+
+            Console.WriteLine("Queres inserir as notas de quantos formandos? ");
+            num_formando = Convert.ToInt32(Console.ReadLine());
+            num_formando_exato = num_formando;
+
+            while (num_formando > 0)
+            {
+                Console.WriteLine(" Qual é a nota do formando? ");
+                nota_formando = Convert.ToDouble(Console.ReadLine());
+               
+                total = total + nota_formando;
+
+                num_formando--;
+
+            }
+            Console.WriteLine($" A média das notas é: {total / num_formando} ");                      
+            Console.ReadKey();
+        }                           
+    }
+}
