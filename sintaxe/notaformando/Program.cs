ca07ef0eca788e2e4df046c1dca970b643ffe750
Sako Afonso Miezi Vuna <61125457+Afonso-sk@users.noreply.github.com>
2022-04-22 10:44:49 +0100

    Sextafeira220420221044

--- sintaxe/notaformando/Program.cs
+++ sintaxe/notaformando/Program.cs
@@ -12,26 +12,27 @@ namespace notaformando
     {
         static void Main(string[] args)
         {
-            int num_formando, num_formando_exato;
+            int num_formando, num_formando_exato; // acrescentei o num_formando_exato porque não estava a dar valor exato ao dividir o total.
             double nota_formando;
-            double media;
             double total = 0;
 
-            Console.WriteLine("Queres inserir as notas de quantos formandos? ");
+            Console.WriteLine("Queres inserir as notas de quantos formandos? ");  
             num_formando = Convert.ToInt32(Console.ReadLine());
-            num_formando_exato = num_formando;
+            num_formando_exato = num_formando; // igualei o num_formando_exado a num_formando para divisão dar um valor correto
 
             while (num_formando > 0)
             {
                 Console.WriteLine(" Qual é a nota do formando? ");
                 nota_formando = Convert.ToDouble(Console.ReadLine());
-               
+
                 total = total + nota_formando;
+
 
                 num_formando--;
 
             }
-            Console.WriteLine($" A média das notas é: {total / num_formando} ");                      
+            Console.WriteLine($" o total é {total}\n");
+            Console.WriteLine($"{total/num_formando_exato}");                     
             Console.ReadKey();
         }                           
     }
