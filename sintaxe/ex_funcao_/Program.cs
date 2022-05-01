9378484da0faa5bb843fdb5795ab726eebf5df6e
Afonso <tec.sakovuna@gmail.com>
2022-05-01 18:40:22 +0100

    segunda 01/05/202219hs

---
+++ sintaxe/ex_funcao_/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_funcao_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome_formando, apelido_formando;
+            int idade_formando;
+
+            Console.WriteLine("Qual é o nome do formando ?");
+            nome_formando = Console.ReadLine();
+
+
+            Console.WriteLine("Qual é o apelido do formando ?");
+            apelido_formando = Console.ReadLine();
+
+
+            Console.WriteLine("Qual é a idade do formando ?");
+            idade_formando = Convert.ToInt32(Console.ReadLine());
+
+            //passar o parametro para guardar
+            ficha_formando(nome_formando, apelido_formando, idade_formando);
+            Console.ReadKey();
+        }
+
+        // As voides não devolvem nada no ecrã
+
+        static void ficha_formando(string nome, string apelido, int idade)
+        {
+            Console.WriteLine($"{nome}  {apelido} tem {idade} anos");
+        }
+    }
+}
---
+++ sintaxe/funcoes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // aqui chamamos a função soma
+            Console.WriteLine($" A soma de 4 + 5 é {soma(4, 5)} e subtração é {subtracao(4 , 5)}");
+            Console.WriteLine($" A soma de 10 + 5 é {soma(4, 5)} e subtração é {subtracao(10, 5)}");
+            Console.ReadKey();
+        }
+        // Criar uma função nossa e essa função pode ser chamada qts vzs quisermos
+         static int soma (int x , int y)
+        {
+            return x + y;
+        }
+
+        static int subtracao(int x, int y)
+        {
+            return x - y;
+        }
+
+
+    }
+}
