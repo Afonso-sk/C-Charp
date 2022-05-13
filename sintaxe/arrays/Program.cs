314887511b0a97d6dd435bb1cc6796ee62a388ae
Sako Afonso Miezi Vuna <61125457+Afonso-sk@users.noreply.github.com>
2022-05-13 15:53:58 +0100

    sextafeira13/05/2022

---
+++ sintaxe/arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Arrays : 
+namespace arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[5];
+
+             
+            valores[0] = 10;
+            valores[1] = 100;
+            valores[2] = 40;
+            valores[3] = 28;
+            valores[4] = 35;
+
+            for(int i = 0; i < valores.Length; i++)
+
+            {
+                Console.Write($"{valores[i]} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sintaxe/arrays_ex1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Criar um programa para gearar numeros aleatórios e apresentar os minimos e maximos
+namespace arrays_ex1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int nums, min, max;
+            Console.WriteLine("Quantos números aleatorios pretendes gerar ?   ");
+            nums = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual é o minimo dos numeros a gerar ?   ");
+            min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual é o máximo dos números a gerar ?   ");
+            max = Convert.ToInt32(Console.ReadLine());
+
+
+            int[] valores = new int[nums];
+            
+            Random random = new Random();
+
+            for (int i = 0; i < valores.Length; i++)
+
+            {
+                valores[i] = random.Next(min, max);
+            }
+
+            for (int i = 0; i < valores.Length; i++)
+
+            {
+                Console.Write(valores[i]);
+            }
+
+            Console.WriteLine("\n -------- Pares ------\n ");
+             
+            for (int i = 0; i < valores.Length; i++)
+
+            {
+                if (valores[i] % 2 == 0)
+                {
+                    Console.Write($"{valores[i]} ");
+                }
+            }
+
+            Console.WriteLine("\n -------- impares ------\n ");
+
+            for (int i = 0; i < valores.Length; i++)
+
+            {
+                if (valores[i] % 2  != 0)
+                {
+                    Console.Write($"{valores[i]} ");
+                }
+            }
+
+            Console.WriteLine("\n -------- par/impares ------\n ");
+
+            for (int i = 0; i < valores.Length; i++)
+
+            {
+                if (valores[i] % 2 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.ForegroundColor= ConsoleColor.Green;
+                }
+
+                Console.Write($"{valores[i]}  ");
+            }
+
+
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
