414f56bdba36b37de6843eb67c0ef123ad5be960
Sako Afonso Miezi Vuna <61125457+Afonso-sk@users.noreply.github.com>
2022-05-20 15:24:43 +0100

    Sextafeira20/05/2022

---
+++ sintaxe/OOP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+/*Fazer um programa para ler as medidads dos lados de dois triangulos  x e y, em seguida , mostrar
+ o valoor das áreas dls dois traingulos e dizer qual dos dois possui a maior area*/
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração da classe do tipo triangulo
+            Triangulo x, y;
+            // declar só não basta, temos que instanciar.
+
+            x = new Triangulo(); // x recebe new triangulo
+            y = new Triangulo();//y recebe novo triangulo
+
+            Console.WriteLine("Entre com as medidads do traingulo X");
+            // para acessar o atributo do seu objeto x é:
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); // isso faz a leitura do primeiro lado do triangulo X
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(" Entre com as medidas do triangulo Y ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (y .A + x.B + x.C) / 2.0;
+            double areax = Math.Sqrt(P * (P - xA) * (P - xB) * (P - xC));
+        }
+    }
+}
---
+++ sintaxe/OOP/Triangulo.cs
@@ -0,0 +1,16 @@
+﻿//Criar classe, clicar a cima de projeto direito, add, class, dá um nome.
+/*Fazer um programa para ler as medidads dos lados de dois triangulos  x e y, em seguida , mostrar
+ o valoor das áreas dls dois traingulos e dizer qual dos dois possui a maior area*/
+
+
+namespace OOP
+{
+    //Criação da classe triangulo.
+    internal class Triangulo
+    {
+        //Public significa que esses atributos poderão ser acessados por outros arquivos.
+        public double A; // double pq o numero poder ser quebrado
+        public double B;
+        public double C;
+    }
+}
